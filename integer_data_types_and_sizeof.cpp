d4222aa055cfa37b409a1eb8a37765b8bf63f3aa
Xie Caicheng <87680698+YoungBossX@users.noreply.github.com>
2025-07-12 16:25:55 +0800

    commit

---
+++ integer_data_types_and_sizeof.cpp
@@ -0,0 +1,39 @@
+#include "iostream"
+
+using namespace std;
+
+#define PI 3.14159 
+
+const int count = 10;
+
+int main(){
+    // short、long、int、long long
+    short age = 18;
+    cout << "年龄是：" << age << endl;
+
+    int num1 = 10;
+    long num2 = 20;
+    long long num3 = 30;
+    cout << "num1: " << num1 << ", num2: " << num2 << ", num3: " << num3 << endl;
+
+    // sizeof()函数，用法sizeof(数据)
+    cout << "short变量，占用字节：" << sizeof(age) << endl;
+    cout << "int变量，占用字节：" << sizeof(num1) << endl;
+    cout << "long变量，占用字节：" << sizeof(num2) << endl;
+    cout << "long long变量，占用字节：" << sizeof(num3) << endl;
+
+    // 整型常量
+    cout << "sizeof(10): " << sizeof(10) << " bytes" << endl;
+    // 实型常量（浮点数）
+    cout << "sizeof(10.0): " << sizeof(10.0) << " bytes" << endl;
+    cout << "sizeof(10.0f): " << sizeof(10.0f) << " bytes" << endl;
+    // 字符常量
+    cout << "sizeof('A'): " << sizeof('A') << " bytes" << endl;
+    // 字符串常量
+    cout << "sizeof(\"A\"): " << sizeof("A") << " bytes" << endl;
+    // 符号常量
+    cout << "sizeof(PI): " << sizeof(PI) << " bytes" << endl;
+    cout << "sizeof(count): " << sizeof(count) << " bytes" << endl;
+
+    return 0;
+}   
---
+++ signed_and_unsigned_integers_demo.cpp
@@ -0,0 +1,29 @@
+#include "iostream"
+
+using namespace std;
+using U_short = unsigned short;
+using U_long = unsigned long;
+using U_ll = unsigned long long;
+
+#define PI 3.14159 
+
+const int count = 10;
+
+int main(){
+    // 有符号：signed（可选），无符号：unsigned（必写）
+    signed int num1 = 10;
+    int num2 = -10;
+    cout << "num1: " << num1 << ", num2: " << num2 << endl;
+
+    unsigned int num3 = -30;
+    cout << "num3: " << num3 << endl;
+
+    // long 无符号
+    unsigned long num4 = 50;
+    // 无符号int、short、long、long long有快捷写法
+    U_short num5 = 100;
+    U_long num6 = 200;
+    U_ll num7 = 300;
+    cout << "num4: " << num4 << ", num5: " << num5 << ", num6: " << num6 << ", num7: " << num7 << endl;
+    return 0;
+}   
