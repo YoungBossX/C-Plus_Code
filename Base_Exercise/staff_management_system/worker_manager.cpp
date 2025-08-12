8180a18767cf1a9803aed8ff6440aa5c6b12b694
XCC <873107451@qq.com>
2025-08-12 23:27:38 +0800

    commit

---
+++ Base_Exercise/staff_management_system/worker_manager.cpp
@@ -0,0 +1,18 @@
+/*
+ * 创建管理类
+ *
+ * 管理类负责的内容如下：
+ *  - 与用户的沟通菜单界面
+ *  - 对职工增删改查的操作
+ *  - 与文件的读写交互
+ */
+
+#include "worker_manager.h"
+
+WorkerManager::WorkerManager() {
+    
+}
+
+WorkerManager::~WorkerManager() {
+
+}
---
+++ Base_Exercise/staff_management_system/worker_manager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+
+class WorkerManager {
+public:
+
+	//构造函数
+	WorkerManager();
+
+	//析构函数
+	~WorkerManager();
+
+};
